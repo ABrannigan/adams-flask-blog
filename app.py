97ba777e803c75669354f8ed48f375a206b1b829
ABrannigan <abran139@gmail.com>
2019-10-19 20:00:10 +0100

    added about route

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 # app.config['DEBUG'] = True
 
 @app.route('/')
-def hello():
-    return "<h1>Hello World!</h1>"
+@app.route('/home')
+def home():
+    return "<h1>Home Page</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>About Page</h1>"
 
 if __name__ == '__main__':
     app.run()
