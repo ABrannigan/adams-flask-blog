6ad91a1bab82bb2940d46136e141dc76d3fce1ea
ABrannigan <abran139@gmail.com>
2019-10-22 11:33:43 +0100

    added showcase posts nav-bar & side-bar

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 # app.config['DEBUG'] = True
+
+posts = [
+        {'author': 'Adam Brannigan',
+         'title': 'blog post 1',
+         'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus voluptatibus necessitatibus laborum, officiis reprehenderit beatae suscipit, natus in incidunt possimus sequi adipisci a, excepturi vitae odio, porro cum saepe est.',
+         'date': '24 Jan 2018',
+         'category': 'Design',
+         'image': 'design.jpg'
+        },
+        {'author': 'Dayna O\'Reilly',
+         'title': 'blog post 2',
+         'content': 'Lorem ipsum dolor sit amet,consectetur adipisicing elit. Repellendus voluptatibus necessitatibus laborum, officiis reprehenderit beatae suscipit, natus in incidunt possimus sequi adipisci a, excepturi vitae odio, porro cum saepe est.',
+         'date': '2 Aug 2018',
+         'category': 'Codeing',
+         'image': 'coding.jpg'
+        }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run()
