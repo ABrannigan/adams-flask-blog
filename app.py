0212f3ee4fb589a340ae2c5e237763afde3ace0d
ABrannigan <abran139@gmail.com>
2019-10-25 14:18:48 +0100

    added dependaniees and env Var

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, flash
 from forms import RegistrationForm, LoginForm
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # local
